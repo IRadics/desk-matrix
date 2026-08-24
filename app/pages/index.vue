08b4f9d1e96a3e03880af925f3135a67185e6e37
István Radics <radics_istvan@hotmail.com>
2026-08-24 18:21:49 +0200

    Reverted coming soon page, updated MW url

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,10 +25,28 @@ const licenseModalOpen = ref<boolean>(false)
         <template #description>
             <div class="flex flex-col gap-6">
                 <div class="flex flex-col gap-1">
-                    <span class="my-8 text-6xl text-white flex gap-4 justify-center">
-                        <UIcon name="i-lucide-rocket"/>
-                        COMING SOON
-                    </span>
+                    <span>A multi piece desk mounted solution for MultiBoard, which provides a stable structure, minimized flexing and full modularity</span>
+                    <span>A comprehensive alternative system to wall mounting.</span>
+                    <span class="mt-4">Create your plan here then 3D print from MakerWorld</span>
+                </div>
+                <div class="flex gap-4 justify-center items-center flex-col md:flex-row">
+                    <UButton 
+                        label="DeskMatrix Planner"
+                        to="/planner"
+                        color="primary"
+                        icon="i-lucide-grid-3x3"
+                        class="py-4 px-6 text-base md:text-xl font-bold justify-center"
+                        size="xl"
+                    />
+                    <UButton 
+                        label="Download from MakerWorld"
+                        to="https://makerworld.com/en/models/3214047-deskmatrix-desk-mount-framework-for-multiboard#profileId-3638506"
+                        target="_blank"
+                        icon="i-custom-maker-world"
+                        class="[&_svg]:text-white! py-4 px-6 text-base md:text-xl font-bold justify-center"
+                        size="xl"
+                        variant="outline"
+                    />
                 </div>
             </div>
         </template>
@@ -38,9 +56,29 @@ const licenseModalOpen = ref<boolean>(false)
             </div>
         </template>
         <template #bottom>
+            <div class="flex justify-center">
+                <UButton 
+                    label="Support me"
+                    icon="i-lucide-heart-plus" 
+                    size="xl" 
+                    class="px-6 justify-center "
+                    variant="ghost"
+
+                    @click="supportModalOpen = true"
+                />
+                <UButton 
+                    label="License"
+                    icon="i-lucide-creative-commons" 
+                    size="xl" 
+                    class="px-6 justify-center"
+                    variant="ghost"
+                    @click="licenseModalOpen = true"
+                />
+            </div>
             <div class="flex justify-center w-full italic text-white/50 my-2 px-4">
                 <span>DeskMatrix is an independent, unofficial community project. It is not affiliated with, sponsored by, or endorsed by MultiBuild, Multiboard LTD, or Keep Making.</span>
             </div>
+
         </template>
     </UPageHero>
     <SupportDialog  v-model:open="supportModalOpen"/>
